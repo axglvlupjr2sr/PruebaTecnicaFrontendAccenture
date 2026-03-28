85482e4cbd42f27ce78874cbb9a5d6ba839e446e
Abi-ezer Guerra <guerra.cuadros25@gmail.com>
2026-03-27 20:47:20 -0500

    chore: add Firebase project config and Remote Config template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    apiKey: 'AIzaSyAngwUXO3ZArojAA9lX31iU2qHJoSJnmJI',
+    authDomain: 'prueba-tecnica-todo-app.firebaseapp.com',
+    projectId: 'prueba-tecnica-todo-app',
+    storageBucket: 'prueba-tecnica-todo-app.firebasestorage.app',
+    messagingSenderId: '236466568039',
+    appId: '1:236466568039:web:55d48a842f23ef774f8dc2',
   }
 };
